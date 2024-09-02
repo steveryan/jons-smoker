10a05c8e5d161f5133eff91c143f2df1eb1ee84a
Steve Ryan <steveryan@github.com>
2024-09-02 09:27:07 -0400

    get rid of the puts and one-line the chart_data generation

--- app/controllers/controls_controller.rb
+++ app/controllers/controls_controller.rb
@@ -5,10 +5,9 @@ class ControlsController < ApplicationController
 
   def chart
     @chart_data = REDISLABS.get("chart")
-    @chart_data = @chart_data.split(" ")
-    @chart_data = @chart_data.map { |x| x.split(",") }
-    @chart_data = @chart_data.map { |x| [x[0], x[1].to_i] }
-    p @chart_data
+    @chart_data = @chart_data.split(" ").map { |x| x.split(",") }.map { |x| [x[0], x[1].to_i] }
+    
+
     render partial: "chart", locals: { chart_data: @chart_data }
   end
 end
